c8ab358a821857d80d6d0780815838c8ffed91c3
xiaofeng <m17620165420@163.com>
2022-03-18 19:15:29 +0800

    delete debug dir

--- cmake-build-debug/src/adtsHeader/CMakeFiles/adtsHeader.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/adtsHeader.dir/adtsHeader.cpp.obj"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/adtsHeader.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/src/decoder/CMakeFiles/decoder.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/decoder.dir/decoder.cpp.obj"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/decoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
